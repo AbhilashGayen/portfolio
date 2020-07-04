8211f6f46e0f43201455bc26d4b867e666789760
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-07-04 11:30:29 +0530

    Refactoring about and index page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Img from "gatsby-image"
-import about from "../images/about.png"
 import About from "../components/about/about"
 
 const AboutPage = () => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-
-import styled from "styled-components"
 import Landing from "../components/landing/landing"
-
-const BaseStyle = styled.html`
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-`
 
 const IndexPage = () => (
   <Layout>
