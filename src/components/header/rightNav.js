8d99574c36a9b4f429686beeeee246c3afc69a57
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-07-04 11:28:42 +0530

    Removing console error

--- src/components/header/rightNav.js
+++ src/components/header/rightNav.js
@@ -6,27 +6,22 @@ const RightNav = ({ open, onClick }) => {
   return (
     <Ul open={open} onClick={() => onClick(!open)}>
       <li>
-        <Link className="link" exact to="/" activeClassName="active-link">
+        <Link className="link" to="/" activeClassName="active-link">
           Home
         </Link>
       </li>
       <li>
-        <Link exact className="link" to="/about" activeClassName="active-link">
+        <Link className="link" to="/about" activeClassName="active-link">
           About
         </Link>
       </li>
       <li>
-        <Link className="link" exact to="/work" activeClassName="active-link">
+        <Link className="link" to="/work" activeClassName="active-link">
           Work
         </Link>
       </li>
       <li>
-        <Link
-          className="link"
-          exact
-          to="/contact"
-          activeClassName="active-link"
-        >
+        <Link className="link" to="/contact" activeClassName="active-link">
           Contact
         </Link>
       </li>
