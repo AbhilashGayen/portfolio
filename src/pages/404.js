dafa82bf945229c12c98adc14082b6498ca0a13c
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-07-05 22:42:18 +0530

    Added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,35 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import "./404.css"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section class="page_404">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 ">
+            <div class="col-sm-10 col-sm-offset-1  text-center">
+              <div class="four_zero_four_bg">
+                <h1 class="text-center ">404</h1>
+              </div>
+              <div class="contant_box_404">
+                <h3 class="h2">Look like you're lost..</h3>
+
+                <p>The page you are looking for is not avaible!</p>
+
+                <Link to="/" class="link_404">
+                  Go to Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
