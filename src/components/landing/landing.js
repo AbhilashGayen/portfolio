88af5d34a3e5dd0add325bc2cb88196f7c815982
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-07-06 19:22:58 +0530

    New Landing Page Layout

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,8 +1,6 @@
 import React from "react"
-import landing from "../../images/landing.svg"
 import ForwardLink from "../forwardLink"
-
-import { Container, Div, Left, Right, ButtonContainer } from "./styles"
+import { Container, Left, ButtonContainer } from "./styles"
 import Button from "../button/button"
 import { motion } from "framer-motion"
 
@@ -10,45 +8,40 @@ const Landing = () => {
   return (
     <>
       <Container>
-        <Div>
-          <Left>
-              <h4>Hi there, I am</h4>
-            <motion.div
-              initial={{ y: "5vw", opacity: 0 }}
-              animate={{ y: 0, opacity: 1, skew: 0 }}
-              transition={{ delay: 0.2, duration: 1 }}
-            >
-              <h1>
-                Abhilash
-                <span className="color wave">!</span>
-              </h1>
-            </motion.div>
-            <motion.div
-              initial={{ y: "10vw", opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ delay: 0.5, duration: 1 }}
-            >
-              <p>
-                Frontend <span className="color">Developer</span>, <br />
-                Software <span className="color">Engineer</span>,
-                <br />
-                and working at <span className="color">Wipro</span>.
-              </p>
-            </motion.div>
-            <motion.div
-              initial={{ x: "10vw", opacity: 0 }}
-              animate={{ x: 0, opacity: 1}}
-              transition={{ delay: 1, duration: 1 }}
-            >
-              <ButtonContainer>
-                <Button />
-              </ButtonContainer>
-            </motion.div>
-          </Left>
-          <Right>
-            <img src={landing} alt="Landing" />
-          </Right>
-        </Div>
+        <Left>
+          <h4>Hi there, I am</h4>
+          <motion.div
+            initial={{ y: "5vw", opacity: 0 }}
+            animate={{ y: 0, opacity: 1, skew: 0 }}
+            transition={{ delay: 0.2, duration: 1 }}
+          >
+            <h1>
+              Abhilash Gayen
+              <span className="color wave">!</span>
+            </h1>
+          </motion.div>
+          <motion.div
+            initial={{ y: "10vw", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
+            <p>
+              Frontend <span className="color">Developer</span>, <br />
+              Software <span className="color">Engineer</span>,
+              <br />
+              and working at <span className="color">Wipro</span>.
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{ x: "10vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
+          >
+            <ButtonContainer>
+              <Button />
+            </ButtonContainer>
+          </motion.div>
+        </Left>
       </Container>
       <ForwardLink label="about" navigate="/about" />
     </>
--- src/components/landing/styles.js
+++ src/components/landing/styles.js
@@ -1,4 +1,5 @@
 import styled, { keyframes } from "styled-components"
+import landing from "../../images/landing1.svg"
 
 const wave = keyframes`
   0% {
@@ -25,20 +26,32 @@ const wave = keyframes`
 `
 
 export const Container = styled.div`
-  height: auto;
+  height: 100vh;
   width: auto;
   margin: 0;
   padding: 0;
+  background: white;
+  background-image: url(${landing});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: 100% 100%;
+  background-size: 50% 90%;
+
+  @media (max-width: 720px) {
+    background-size: 90%;
+    background-position: 115px 300px;
+  }
 `
 
 export const Div = styled.div`
-  width: 80%;
+  width: 90%;
   height: auto;
   overflow: hidden;
   margin: auto;
   padding: auto 5rem;
   display: flex;
-  flex-direction: row;
+  align-content: center;
+
   @media (max-width: 720px) {
     width: 90%;
     padding: 0;
@@ -47,23 +60,22 @@ export const Div = styled.div`
 `
 
 export const Left = styled.div`
-  float: left;
-  width: 45%;
+  position: fixed;
+  top: 30%;
+  left: 15%;
+  width: 80%;
   height: auto;
-  margin: 1rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  margin: auto;
 
   h1 {
     padding-top: 0.5rem;
     display: inline-block;
     font-size: 68px;
     line-height: 1.1;
-    font-family: "AcuminBold";
+    font-family: "Raleway";
+    font-weight: 600;
     letter-spacing: 0.1rem;
     color: #22223b;
-    opacity: 0.9;
   }
 
   h4 {
@@ -71,14 +83,14 @@ export const Left = styled.div`
     margin: 0;
     display: inline-block;
     font-size: 18px;
-    font-family: "AcuminBold";
+    font-family: "Raleway";
     color: #22223b;
     opacity: 0.8;
   }
 
   p {
     color: #242423;
-    font-size: 42px;
+    font-size: 36px;
     line-height: 1.4;
     font-family: "Klima";
     letter-spacing: 0.15rem;
@@ -101,9 +113,9 @@ export const Left = styled.div`
   }
 
   @media (max-width: 720px) {
-    text-align: left;
-    width: 100%;
-    margin: 2rem auto 1rem auto;
+    position: absolute;
+    top: 2%;
+    left: 5%;
 
     h4 {
       padding-left: 1vw;
@@ -128,14 +140,14 @@ export const Left = styled.div`
 
 export const Right = styled.div`
   float: right;
-  width: 55%;
+  width: 50%;
   height: auto;
   color: white;
   margin-top: 1rem;
 
   img {
     object-fit: cover;
-    width: 100%;
+    height: 90%;
   }
 
   @media (max-width: 720px) {
